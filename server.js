b097c542c86641aa30ac1b985ecd5c6036d7e426
SassanKermani <SassanKermani2@gmail.com>
2018-10-24 16:51:57 -0600

    ok we may be good

---
+++ server.js
@@ -0,0 +1,43 @@
+/*----------  Setup  ----------*/
+
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+let fs = require("fs");
+
+/*----------  stuff  ----------*/
+
+app.get('/*', (req, res)=>{
+	res.sendfile("./index.html")
+});
+
+app.post('/getSpot', (req,res)=>{
+	// res.send("test");
+	fs.readFile(`theSpot.txt`, (err, data)=>{
+	if(err){
+			throw err; console.log(err); 
+			console.log(`you hit a bug thats neet : ${err}`);
+			res.send("yell as Sassan he wrote bad code");
+		}else{
+			res.send(data);
+		}
+	});
+});
+
+app.post('/newSpot', (req,res)=>{
+	fs.writeFile('theSpot.txt', req.body.spot, function (err) {
+		if (err) throw err;
+		console.log(req.body.spot);
+	});
+	res.send(req.body.spot)
+})
+
+/*----------  listen  ----------*/
+
+app.listen(port, ()=>{
+	console.log(`up at ${port}`);
+});
